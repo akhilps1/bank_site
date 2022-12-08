bdda9f8ac611732fb6b90e2b2e701e55c736812b
akhilps <akhilps595@gmail.com>
2022-12-08 12:39:32 +0530

    production version

--- bank_site/bank_site/settings.py
+++ bank_site/bank_site/settings.py
@@ -80,8 +80,10 @@ WSGI_APPLICATION = 'bank_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'account_opening_form',
+        'USER':'root',
+        'PASSWORD':'',
     }
 }
 
